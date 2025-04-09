a001774e9a8823d8429a8baebc324159a3e1999c
Iasmym <iasmympatricia7@gmail.com>
2025-04-09 19:12:18 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -35,3 +35,58 @@ document.getElementById('formCadastro').addEventListener('submit', function(even
     };
     reader.readAsDataURL(foto);
 });
+
+function atualizarListaClientes() {
+    const listaClientes = document.getElementById('listaClientes');
+    listaClientes.innerHTML = ''; 
+    clientes.forEach((cliente, index) => {
+        const li = document.createElement('li');
+        const img = document.createElement('img');
+        img.src = cliente.foto;
+        
+        const texto = document.createElement('span');
+        texto.textContent = cliente.exibir();
+        const btnEditar = document.createElement('button');
+        btnEditar.textContent = 'edite';
+        btnEditar.classList.add('edit');
+        btnEditar.onclick = () => editarCliente(index);
+
+        const btnExcluir = document.createElement('button');
+        btnExcluir.textContent = 'exclua';
+        btnExcluir.classList.add('delete');
+        btnExcluir.onclick = () => excluirCliente(index);
+
+        li.appendChild(img);
+        li.appendChild(texto);
+        li.appendChild(btnEditar);
+        li.appendChild(btnExcluir);
+        listaClientes.appendChild(li);
+    });
+}
+function editarCliente(index) {
+    const cliente = clientes[index];
+
+    const nome = prompt('renomeiar informação:', cliente.nome);
+    const email = prompt('altere seu email:', cliente.email);
+    const idade = prompt('altere sua idade:', cliente.idade);
+    const foto = prompt('adicione novo link para trocar de imagem:', cliente.foto);
+
+    if (nome && email && idade) {
+        cliente.nome = nome;
+        cliente.email = email;
+        cliente.idade = idade;
+        if (foto) {
+            cliente.foto = foto;
+        }
+    }
+
+    atualizarListaClientes(); 
+}
+
+function excluirCliente(index) {
+    const confirmacao = confirm('deseja mesmo excluir esse cadastro?');
+    if (confirmacao) {
+        clientes.splice(index, 1);
+        atualizarListaClientes(); 
+    }
+}
